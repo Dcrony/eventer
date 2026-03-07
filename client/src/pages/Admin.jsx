376f2d4eca7ca65d83389ac298f656def6aeadca
Ibrahim Abdulmajeed <ibrahimabdulmajeed14@gmail.com>
2026-03-07 22:22:02 +0100

    Change API URL port from 5000 to 8080

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./CSS/UserManagement.css"; // import your CSS file
 
-const PORT_URL = import.meta.env.REACT_APP_API_URL || "http://localhost:5000";
+const PORT_URL = import.meta.env.REACT_APP_API_URL || "http://localhost:8080";
 
 const UserManagement = () => {
   const [users, setUsers] = useState([]);
