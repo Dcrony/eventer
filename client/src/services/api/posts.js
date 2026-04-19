f37d15c53b993a1832a3e15e19b0b4c2e25ccf4f
Dcrony <ibrahimabdulmajeed14@gmail.com>
2026-04-19 14:44:10 +0100

    services

--- client/src/services/api/posts.js
+++ client/src/services/api/posts.js
@@ -4,3 +4,5 @@ export const getPosts = () => API.get("/posts");
 export const createPost = (payload) => API.post("/posts", payload);
 export const likePost = (postId) => API.post(`/posts/${postId}/like`);
 export const commentOnPost = (postId, payload) => API.post(`/posts/${postId}/comments`, payload);
+export const getPostComments = (postId) => API.get(`/comments/${postId}`);
+export const createComment = (payload) => API.post("/comments", payload);
