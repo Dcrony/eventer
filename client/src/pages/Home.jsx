5de49f61b2b393008a8c26ad13db3e324855d202
Ibrahim Abdulmajeed <ibrahimabdulmajeed14@gmail.com>
2026-05-03 00:13:55 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import EventCardSkeleton from "../components/EventCardSkeleton";
 import useDemoEvents from "../hooks/useDemoEvents";
 import useProfileNavigation from "../hooks/useProfileNavigation";
 import "./CSS/home.css";
+import SEO from "../public/SEO"
 
 const EVENT_FILTER_CHIPS = [
   { id: "all", label: "All" },
@@ -62,6 +63,41 @@ export default function Home() {
   const { toProfile } = useProfileNavigation();
 
   const demoEvents = useDemoEvents(events, error && !useDemoData);
+
+<SEO
+  title={`${event.title} | Buy Tickets on TickiSpot`}
+  description={event.description}
+  url={`https://tickispot.com/events/${event.slug}`}
+/>
+
+<script type="application/ld+json">
+{JSON.stringify({
+  "@context": "https://schema.org",
+  "@type": "Event",
+  "name": event.title,
+  "startDate": event.date,
+  "eventAttendanceMode": "https://schema.org/OfflineEventAttendanceMode",
+  "eventStatus": "https://schema.org/EventScheduled",
+  "location": {
+    "@type": "Place",
+    "name": event.venue,
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "Lagos",
+      "addressCountry": "NG"
+    }
+  },
+  "image": [event.image],
+  "description": event.description,
+  "offers": {
+    "@type": "Offer",
+    "url": `https://tickispot.com/events/${event.slug}`,
+    "price": event.price,
+    "priceCurrency": "NGN",
+    "availability": "https://schema.org/InStock"
+  }
+})}
+</script>
 
   useEffect(() => {
     const fetchEvents = async () => {
