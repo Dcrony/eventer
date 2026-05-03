38a269dd772ccd86650aa72bf50d21c4420d817a
Dcrony <ibrahimabdulmajeed14@gmail.com>
2026-05-03 13:53:20 +0100

    add helmet

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import useDemoEvents from "../hooks/useDemoEvents";
 import useProfileNavigation from "../hooks/useProfileNavigation";
 import "./CSS/home.css";
 import SEO from "../../public/SEO";
+import { Helmet } from "react-helmet";
 
 
 
