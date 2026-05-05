97b3aba0e686cc8e51cc6d1cc75363d6c6ba6794
Dcrony <ibrahimabdulmajeed14@gmail.com>
2026-05-05 13:14:02 +0100

    server middleware

--- server/middleware/requirePro.js
+++ server/middleware/requirePro.js
@@ -1,33 +1,7 @@
-const { ensureSubscriptionState, hasProAccess } = require("../services/subscriptionService");
+const { requireFeature } = require("../services/featureService");
 
 const UPGRADE_MESSAGE = "Upgrade to Pro to access this feature";
 
-const requirePro = async (req, res, next) => {
-  try {
-    if (!req.user) {
-      return res.status(401).json({ message: "Authentication required" });
-    }
+const requirePro = requireFeature("TICKI_AI");
 
-    if (req.user.role === "admin") {
-      return next();
-    }
-
-    await ensureSubscriptionState(req.user);
-
-    if (!hasProAccess(req.user)) {
-      return res.status(403).json({
-        code: "PLAN_UPGRADE_REQUIRED",
-        message: UPGRADE_MESSAGE,
-        plan: req.user.plan || "free",
-        trialEndsAt: req.user.trialEndsAt || null,
-      });
-    }
-
-    return next();
-  } catch (error) {
-    console.error("requirePro error", error);
-    return res.status(500).json({ message: "Server error" });
-  }
-};
-
-module.exports = { requirePro, UPGRADE_MESSAGE };
+module.exports = { requirePro, requireFeature, UPGRADE_MESSAGE };
