bedb59ee17e0c0493e0effe82c0453cee1e69c84
Dcrony <ibrahimabdulmajeed14@gmail.com>
2026-05-09 17:20:33 +0100

    add ticki ai route to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import { NotificationsProvider } from "./hooks/useNotifications";
 import { CreateEventProvider } from "./context/CreateEventContext";
 import { getCurrentUser } from "./utils/auth";
 import { getTrialDaysRemaining, isTrialEndingSoon } from "./utils/planAccess";
+import TickiAIChat from "./components/TickiAIChat";
 
 // Loading component for Suspense fallback
 const PageLoader = () => (
@@ -196,6 +197,8 @@ function Layout() {
               <Route path="/founder" element={<FounderProfile />} />
               <Route path="/discover/creators" element={<DiscoverCreators />} />
               <Route path="/user/:username" element={<Profile />} />
+
+              <Route path="/ticki-ai" element={<TickiAIChat />} />
 
               {/* Protected Routes */}
               <Route
