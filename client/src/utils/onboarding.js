e26270277c6668c58c8751b0fbe9401abb7de841
Dcrony <ibrahimabdulmajeed14@gmail.com>
2026-06-10 15:43:52 +0100

    fix bug onboarding

--- client/src/utils/onboarding.js
+++ client/src/utils/onboarding.js
@@ -1,14 +1,4 @@
-// export const getOnboardingStep = (user) => {
-//   if (!user) return null;
-//   if (!user.roleConfirmed) return "role";
-//   if (user.role === "user" && !user.interestsSelected) return "interests";
-//   return null;
-// };
 
-// export const isOnboardingComplete = (user) =>
-//   Boolean(user?.roleConfirmed && (user.role === "organizer" || user.interestsSelected));
-
-// export const shouldPromptOnboarding = (user) => Boolean(getOnboardingStep(user));
 
 
 /**
